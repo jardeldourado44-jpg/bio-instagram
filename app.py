9f64c2a057e6a6ab3272e2137341ef67942cfb2c
Jardel <jardeldourado44@gmail.com>
2026-03-08 16:14:00 -0300

    corrigindo porta render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import random
+import os
 
 app = Flask(__name__)
 
@@ -36,5 +37,7 @@ def home():
 
     return render_template("index.html", bios=bios)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
