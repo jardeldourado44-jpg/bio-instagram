afa14a8600f2c005239d29804fd5a95b6d03d4f6
Jardel <jardeldourado44@gmail.com>
2026-03-08 16:21:04 -0300

    fix render port

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ def home():
         cidade = request.form["cidade"]
 
         for i in range(5):
-
             modelo = random.choice(modelos)
 
             bio = modelo.format(
@@ -38,6 +37,6 @@ def home():
     return render_template("index.html", bios=bios)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT", 10000))
+
+app.run(host="0.0.0.0", port=port)
